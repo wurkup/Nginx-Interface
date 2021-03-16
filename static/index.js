3dd68c4c83dc9ce278f61ccba5c32350292aa45a
sai3010 <sai.prasanth@dailyrounds.org>
2021-03-16 22:56:27 +0530

    added server check

--- static/index.js
+++ static/index.js
@@ -1,4 +1,11 @@
-function start()
-{
-    fetch("/server-start")
+function check_server() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.status != 200) {
+            alert("Nginx not found! Install & restart the UI.")
+            window.top.close();
+        }
+    };
+    xhttp.open("GET", "/server-status", true);
+    xhttp.send();
 }
