de6fe381e12df49cff6e45eda840a972ade3fad7
sai3010 <sai.prasanth@dailyrounds.org>
2021-04-12 14:38:15 +0530

    moved all calls to fetch

--- static/index.js
+++ static/index.js
@@ -1,17 +1,15 @@
 async function check_server() {
     const resp = await fetch('/server-status')
-    var finalres = await resp
-    if (finalres.status != 200) {
+    if (resp.status != 200) {
         alert("Nginx not found! Install & restart the UI.")
     }
-    else if (finalres.status == 200) {
+    else if (resp.status == 200) {
         await list_files()
     }
 }
 async function list_files() {
     const resp = await fetch('/config/list')
-    var finalres = await resp
-    if (finalres.status == 200) {
+    if (resp.status == 200) {
         var jsonResponse = await resp.json()
         for (var i = 0; i < jsonResponse.files.length; i++) {
             var sel = document.getElementById("select-file")
@@ -21,43 +19,38 @@ async function list_files() {
         }
     }
 }
-function get_file() {
+async function get_file() {
     var sel = document.getElementById("select-file")
     var filename = sel.value;
     let url = "/config/i/" + filename
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
-        if (this.status == 200) {
-            var editor = document.getElementById("editor")
-            editor.textContent = this.responseText
-        }
-    };
-    xhttp.open("GET", url, true);
-    xhttp.send();
+    const resp = await fetch(url)
+    if (resp.status == 200) {
+        var editor = document.getElementById("editor")
+        editor.textContent = await resp.text()
+    }
 }
-function file_save() {
+async function file_save() {
     var sel = document.getElementById("select-file")
     var filename = sel.value;
     let url = "/config/i/" + filename
-    var xhttp = new XMLHttpRequest();
     var editor = document.getElementById("editor")
     var file_content = {}
     file_content["content"] = editor.value.split("\n")
-    xhttp.open("PUT", url, true);
-    xhttp.setRequestHeader('Content-Type', 'application/json');
-    xhttp.send(JSON.stringify(file_content));
-    xhttp.onreadystatechange = function () {
-        if (this.status == 200) {
-            alert("file saved")
-        }
+    const response = await fetch(url, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(file_content)
+    })
+    if (response.status == 200) {
+        alert("file saved")
     };
 }
 async function test_conf() {
     const resp = await fetch('/config/test')
-    var finalres = await resp
-    if(finalres.status ==200)
-    {
-        var msg = await finalres.json()
+    if (resp.status == 200) {
+        var msg = await resp.json()
         alert(msg.msg)
     }
 }
