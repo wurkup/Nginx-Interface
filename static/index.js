fde9fe3c102e2cdf8cd455c4fa2a2d4c5f3d1b92
sai3010 <sai.prasanth@dailyrounds.org>
2021-04-12 12:35:30 +0530

    moving calls to fetch

--- static/index.js
+++ static/index.js
@@ -1,37 +1,30 @@
-function check_server() {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
-        if (this.status != 200) {
-            alert("Nginx not found! Install & restart the UI.")
-            list_files()
-        }
-    };
-    xhttp.open("GET", "/server-status", true);
-    xhttp.send();
+async function check_server() {
+    const resp = await fetch('/server-status')
+    var finalres = await resp
+    if (finalres.status != 200) {
+        alert("Nginx not found! Install & restart the UI.")
+    }
+    else if (finalres.status == 200) {
+        await list_files()
+    }
 }
-function list_files()
-{
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
-        if (this.status == 200) {
-            var jsonResponse = JSON.parse(this.responseText);
-            for(var i=0;i<jsonResponse.files.length;i++)
-            {
-                var sel = document.getElementById("select-file")
-                var op = document.createElement("option")
-                op.value,op.text=jsonResponse.files[i]
-                sel.appendChild(op)
-            }
+async function list_files() {
+    const resp = await fetch('/config/list')
+    var finalres = await resp
+    if (finalres.status == 200) {
+        var jsonResponse = await resp.json()
+        for (var i = 0; i < jsonResponse.files.length; i++) {
+            var sel = document.getElementById("select-file")
+            var op = document.createElement("option")
+            op.value, op.text = jsonResponse.files[i]
+            sel.appendChild(op)
         }
-    };
-    xhttp.open("GET", "/config/list", true);
-    xhttp.send();
+    }
 }
-function get_file()
-{
+function get_file() {
     var sel = document.getElementById("select-file")
     var filename = sel.value;
-    let url = "/config/i/"+filename
+    let url = "/config/i/" + filename
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
         if (this.status == 200) {
@@ -42,31 +35,29 @@ function get_file()
     xhttp.open("GET", url, true);
     xhttp.send();
 }
-function file_save()
-{
+function file_save() {
     var sel = document.getElementById("select-file")
     var filename = sel.value;
-    let url = "/config/i/"+filename
+    let url = "/config/i/" + filename
     var xhttp = new XMLHttpRequest();
     var editor = document.getElementById("editor")
     var file_content = {}
-    file_content["content"] = editor.value.split("\n")   
+    file_content["content"] = editor.value.split("\n")
+    xhttp.open("PUT", url, true);
+    xhttp.setRequestHeader('Content-Type', 'application/json');
+    xhttp.send(JSON.stringify(file_content));
     xhttp.onreadystatechange = function () {
         if (this.status == 200) {
             alert("file saved")
         }
     };
-    xhttp.open("PUT", url, true);
-    xhttp.setRequestHeader('Content-Type', 'application/json');
-    xhttp.send(JSON.stringify(file_content));
 }
-function test_conf()
-{
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
-        var msg = JSON.parse(this.responseText)
+async function test_conf() {
+    const resp = await fetch('/config/test')
+    var finalres = await resp
+    if(finalres.status ==200)
+    {
+        var msg = await finalres.json()
         alert(msg.msg)
-    };
-    xhttp.open("GET", "config/test", true);
-    xhttp.send();
+    }
 }
